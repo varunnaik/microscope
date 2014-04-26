d30c6d56194050c381e6535225f6dbb6731485f1
Varun Naik <varun.naik@gmail.com>
2014-04-27 00:10:36 +0530

    Add comments collection, pub/sub, and fixtures.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,41 @@
 if (Posts.find().count() === 0) {
-    Posts.insert({
-	title: 'Introducing Telescope',
-	author: 'Sacha Greif',
-	url: 'http://sachagreif.com/introducing-telescope/'
+    var now = new Date().getTime();
+
+    // Create two users
+    var tomId = Meteor.users.insert({
+	profile: {name: 'Tom Coleman'}
     });
+    var tom = Meteor.users.findOne(tomId);
+    var sachaId = Meteor.users.insert({
+	profile: {name: 'Sacha Greif'}
+    });
+    var sacha = Meteor.users.findOne(sachaId);
+
+    var telescopeId = Posts.insert({
+	title: 'Introducing Telescope',
+	userId: sacha._id,
+	author: sacha.profile.name,
+	url: 'http://sachagreif.com/introducing-telescope/',
+	submitter: now - 7 * 3600 * 1000
+    });
+
+    Comments.insert({
+	postId: telescopeId,
+	userId: tom._id,
+	author: tom.profile.name,
+	submitted: now - 5 * 3600 * 1000,
+	body: "Interesting project Sacha, can I get involved?"
+    });
+
+    Comments.insert({
+	postId: telescopeId,
+	userId: sacha._id,
+	author: sacha.profile.name,
+	submitted: now - 3 * 3600 * 1000,
+	body: 'You sure can Tom!'
+    });
+
+
     Posts.insert({
 	title: 'Meteor',
 	author: 'Tom Coleman',
